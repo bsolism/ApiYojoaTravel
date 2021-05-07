57f401e2e21bf3ab2b7358ec44a19dc11c5ab21d
Bernardo Solis <bsolis@unitec.edu>
2021-05-06 23:04:19 -0600

    Add-UnitTest

--- ApiYojoaTravel/DomainService/ActivityDomainService.cs
+++ ApiYojoaTravel/DomainService/ActivityDomainService.cs
@@ -1,4 +1,5 @@
 using ApiYojoaTravel.Models;
+using System;
 
 namespace ApiYojoaTravel.DomainService
 {
@@ -9,8 +10,8 @@ namespace ApiYojoaTravel.DomainService
             bool requiredField;
             requiredField = (activity.Name == null) ? true : false;
             requiredField = (activity.Description == null) ? true : false;
-            requiredField = (activity.InitTime == null) ? true : false;
-            requiredField = (activity.EndTime == null) ? true : false;
+            requiredField = (activity.InitTime == new DateTime(0)) ? true : false;
+            requiredField = (activity.EndTime == new DateTime(0)) ? true : false;
             requiredField = (activity.Price == 0) ? true : false;
             requiredField = (activity.ImageURL == null) ? true : false;
             return requiredField;
